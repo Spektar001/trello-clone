bcd93d8a596b9e7100c5b5fe334a03e9b1c33068
Harry-Gupenec <Spektar001@mail.ru>
2024-02-16 10:16:54 +0300

    refactor: change css style

--- src/components/views/CardModal.tsx
+++ src/components/views/CardModal.tsx
@@ -28,7 +28,7 @@ const CardModal = () => {
         className="absolute inset-0 z-20 w-full"
         onClick={handleBackDropClick}
       >
-        <div className="max-w-md mx-auto bg-white my-8 px-4 p-1 rounded-md">
+        <div className="max-w-md mx-auto bg-white my-8 p-4 rounded-md">
           <div onClick={(ev) => ev.stopPropagation()}>
             <CardModalBody />
           </div>
