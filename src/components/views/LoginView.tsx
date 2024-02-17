c316078ec1fd6dd9cbe4801b39ded3d307444a7f
Harry-Gupenec <Spektar001@mail.ru>
2024-02-17 14:52:16 +0300

    feat: change btn in the component LoginView

--- src/components/views/LoginView.tsx
+++ src/components/views/LoginView.tsx
@@ -1,13 +1,20 @@
-'use client';
+"use client";
 
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 const LoginView = () => {
   return (
-    <div className="w-full pt-8 text-center">
-      <button onClick={() => signIn('google')} className="primary">Login</button>
+    <div className="w-full flex items-center justify-center mt-8">
+      <button
+        onClick={() => signIn("google")}
+        className="flex items-center justify-center gap-4 bg-white text-gray-900 py-2 px-4 shadow-md rounded-md hover:shadow-sm transition ease-in duration-200"
+      >
+        <Image src={"/google.png"} alt="google" width={24} height={24} />
+        Login with Google
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default LoginView
+export default LoginView;
